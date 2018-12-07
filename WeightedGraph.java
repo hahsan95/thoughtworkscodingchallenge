3a83d395fe293ff5f15fe9524c69567fbd63d06c
Homum Ahsan <hahsan95@gmail.com>
2018-12-07 17:39:18 -0600

    got hasPath working

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -1,5 +1,6 @@
 
 import java.util.LinkedList;
+import java.util.HashMap;
 
 public class WeightedGraph {
     static class Edge {
@@ -43,15 +44,30 @@ public class WeightedGraph {
             }
         }
 
-        public boolean pathDistance(int index, char source, char destination){
+        public int pathDistance(int index, char source, char destination){
             LinkedList<Edge> list = adjacencylist[index];
             for(int i = 0; i < list.size(); i++) {
                 if(list.get(i).destination == destination){
-                    System.out.println(list.get(i).weight);
-                    return true;
+                    return list.get(i).weight;
                 }
             }
-            return false;
+            return 0;
+        }
+
+        public void hasPath(String path){
+            HashMap<Character, Integer> trainIndex = new HashMap<Character, Integer>();
+            trainIndex.put('A', 0);
+            trainIndex.put('B', 1);
+            trainIndex.put('C', 2);
+            trainIndex.put('D', 3);
+            trainIndex.put('E', 4);
+            int distance = 0;
+            for(int i = 0; i <= path.length() - 3; i += 2){
+                char beginning = path.charAt(i);
+                char end = path.charAt(i+2);
+                distance += pathDistance(trainIndex.get(beginning), beginning, end);
+                System.out.println(distance);
+            }
         }
     }
       public static void main(String[] args) {
@@ -67,12 +83,12 @@ public class WeightedGraph {
             trainRoutes.addEdge(4, 'E', 'B', 3);
             trainRoutes.addEdge(0, 'A', 'E', 7);
             // trainRoutes.printGraph();
-            int weight = 0;
-            boolean test = trainRoutes.pathDistance(0, 'A', 'B');
-            if(test == true) {
-                System.out.println('P');
-                trainRoutes.pathDistance(1, 'B', 'C');
-            }
+            trainRoutes.hasPath("A-E-B-C-D");
+            // boolean test = trainRoutes.pathDistance(0, 'A', 'B');
+            // if(test == true) {
+            //     System.out.println('P');
+            //     trainRoutes.pathDistance(1, 'B', 'C');
+            // }
 
       }
 }
