eff440ef24e0024107fc0f28ae7516638d402117
Homum Ahsan <hahsan95@gmail.com>
2018-12-07 21:56:13 -0600

    fixed cart items

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -35,18 +35,18 @@ public class ShoppingCart {
 
     Cart cart1 = new Cart();
     cart1.addItem(1, "book", 12.49F, true, false);
-    cart1.addItem(1, "book", 12.49F, true, false);
-    cart1.addItem(1, "book", 12.49F, true, false);
+    cart1.addItem(1, "music CD", 14.99F, false, false);
+    cart1.addItem(1, "chocolate bar", 0.85F, true, false);
 
     Cart cart2 = new Cart();
-    cart2.addItem(1, "book", 12.49F, true, false);
-    cart2.addItem(1, "book", 12.49F, true, false);
+    cart2.addItem(1, "box of chocolates", 10.00F, true, true);
+    cart2.addItem(1, "bottle of perfume", 47.50F, false, true);
 
     Cart cart3 = new Cart();
-    cart3.addItem(1, "book", 12.49F, true, false);
-    cart3.addItem(1, "book", 12.49F, true, false);
-    cart3.addItem(1, "book", 12.49F, true, false);
-    cart3.addItem(1, "book", 12.49F, true, false);
+    cart3.addItem(1, "bottle of perfume", 27.99F, false, true);
+    cart3.addItem(1, "bottle of perfume", 18.99F, false, false);
+    cart3.addItem(1, "packet of headache pills", 9.75F, true, false);
+    cart3.addItem(1, "box of chocolates", 11.25F, true, true);
     System.out.println(cart1.currentCart.get(0).name);
   }
 }
